cd450f53fea59837d628818fe0d70c861f82ead5
magenabraham <joe+ma@ticc.net>
2022-04-25 16:00:22 -0400

    Initial Website Structure

--- app.py
+++ app.py
@@ -3,5 +3,37 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<p>Home Page</p>"
+
+@app.route("/classes")
+def classes():
+    return "<p>Classes</p>"
+
+@app.route("/contact")
+def contact():
+    return "<p>Contact</p>"
+
+@app.route("/donate")
+def donate():
+    return "<p>Donate</p>"
+
+@app.route("/faq")
+def faq():
+    return "<p>FAQ</p>"
+
+@app.route("/gallery")
+def gallery():
+    return "<p>Gallery</p>"
+
+@app.route("/press")
+def press():
+    return "<p>Press</p>"
+
+@app.route("/schedule")
+def schedule():
+    return "<p>Schedule</p>"
+
+@app.route("/sports")
+def sports():
+    return "<p>Sports</p>"
